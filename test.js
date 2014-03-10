3708941fe7b1aa530cd9552a2e3692ef7e2483da
Jeremie Miller <jeremie@jabber.org>
2014-03-10 14:21:48 -0600

    fixed the dummy test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var cs = require("./node.js");
 
 // dummy functions
-cs.install({CSets:{},pdecode:function(){console.log("pdecode",arguments);return {}},pencode:function(){console.log("pencode",arguments);return new Buffer(0)}});
+cs.install({CSets:{},pdecode:function(){console.log("pdecode",arguments);return {}},pencode:function(){console.log("pencode",arguments);return new Buffer(1)}});
 
 var a = {parts:{}};
 var b = {parts:{}};
@@ -10,10 +10,10 @@ cs.genkey(a,function(){
   console.log("genkey",a);
   cs.genkey(b,function(){
     console.log("genkey",b);
-    var id = {cs:{"2a":{}}};
-    cs.loadkey(id.cs["2a"],a["2a"],a["2a_secret"]);
+    var id = {cs:{"3a":{}}};
+    cs.loadkey(id.cs["3a"],a["3a"],a["3a_secret"]);
     var to = {lineOut:"00"};
-    cs.loadkey(to,b["2a"]);
+    cs.loadkey(to,b["3a"]);
     console.log(id,to);
     var open = cs.openize(id,to,{});
     console.log("opened",open);
