6f5b2aad3b0d49f2622c5091f1c7f7af1920651b
Jeremie Miller <jeremie@jabber.org>
2014-03-12 17:58:57 -0600

    let it fail gracefully

--- node.js
+++ node.js
@@ -1,5 +1,9 @@
-var sodium = require("sodium").api;
-var cs3a = require("./cs3a.js");
-cs3a.crypt(sodium);
+try{
+  var sodium = require("sodium").api;
+  var cs3a = require("./cs3a.js");
+  cs3a.crypt(sodium);
 
-Object.keys(cs3a).forEach(function(f){ exports[f] = cs3a[f];});
+  Object.keys(cs3a).forEach(function(f){ exports[f] = cs3a[f];});  
+}catch(E){
+  console.log("CS3a failed to load",E);
+}
