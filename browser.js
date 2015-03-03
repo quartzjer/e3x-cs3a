6c9ed796a18731e5cb9550405a61582d5ff901cc
nick <buzzheavyyear@users.noreply.github.com>
2015-03-03 15:04:43 +0000

    js-nacl envelope for browser

--- browser.js
+++ browser.js
@@ -1,4 +1,65 @@
-//var nacl = require("js-nacl");
-//var cs3a = require("./cs3a.js");
-
-// TODO! need to create a sodium-compatible Buffer based api wrapper around js-nacl
+var nacl_factory = require("js-nacl");
+var nacl = nacl_factory.instantiate();
+exports = module.exports = require('./cs3a.js');
+sodium = function()
+{
+     //From Buffer to ArrayBuffer:
+  function toArrayBuffer(buffer) {
+    var ab = new ArrayBuffer(buffer.length);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        view[i] = buffer[i];
+    }
+    return ab;
+  }
+    //From ArrayBuffer to Buffer
+   function toBuffer(ab) {
+    var buffer = new Buffer(ab.byteLength);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        buffer[i] = view[i];
+    }
+    return buffer;
+  }
+  self.crypto_secretbox_BOXZEROBYTES = nacl.crypto_secretbox_BOXZEROBYTES;
+  self.crypto_box_keypair = function(){
+      var keypair = nacl.crypto_box_keypair();
+      return {key:toBuffer(keypair.publicKey), secret:toBuffer(keypair.secretKey)};
+  };
+  self.crypto_box_beforenm = function(publickey, secretkey){
+      var precomputedsharedkey = nacl.crypto_box_precompute(toArrayBuffer(publickey), toArrayBuffer(secretkey) );
+      return toBuffer(precomputedsharedkey.boxK);
+  };
+  self.crypto_secretbox_open = function(ciphertextBin, nonceBin, keyBin){
+      return toBuffer(
+          nacl.crypto_secretbox_open(
+              toArrayBuffer(ciphertextBin),
+              toArrayBuffer(nonceBin),
+              toArrayBuffer(keyBin)
+          )
+      );
+  }
+  self.crypto_secretbox = function(msgBin, nonceBin, keyBin){
+      return toBuffer(
+          nacl.crypto_secretbox(
+              toArrayBuffer(msgBin),
+              toArrayBuffer(nonceBin),
+              toArrayBuffer(keyBin)
+          )
+      );
+  }
+  self.crypto_onetimeauth = function(message, secretkey){
+      return toBuffer(
+          nacl.crypto_onetimeauth(
+              toArrayBuffer(message),
+              toArrayBuffer(secretkey)
+          )
+      );
+  }
+}
+try{
+  // load node-specific crypto methods
+  exports.crypt(sodium);
+}catch(E){
+  console.log("CS3a failed to load (TODO use tweetnacl.js?):",E);
+}
